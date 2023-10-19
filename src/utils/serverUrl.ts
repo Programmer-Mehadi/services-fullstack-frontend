d49e194682528475b8eb1fa795a2ef67cc9e6d96
ProgrammerShawon <muhammad.shawon.developer@gmail.com>
2023-10-20 05:06:21 +0600

    live url added.

--- src/utils/serverUrl.ts
+++ src/utils/serverUrl.ts
@@ -1,3 +1,6 @@
+// export const serverURL =
+//   (process.env.NEXT_PUBLIC_API_BASE_URL as string) ||
+//   "http://localhost:5000/api/v1";
 export const serverURL =
   (process.env.NEXT_PUBLIC_API_BASE_URL as string) ||
-  "http://localhost:5000/api/v1";
+  "https://elegance-interiors-backend-server.vercel.app/api/v1";
