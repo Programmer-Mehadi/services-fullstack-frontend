4bb14126413a2337295fe5b398c262d8a191a706
Md Zahirul Islam Mehadi <108610227+Programmer-Mehadi@users.noreply.github.com>
2024-01-20 15:28:22 +0600

    [DONE] api changes.

--- src/utils/serverUrl.ts
+++ src/utils/serverUrl.ts
@@ -1,4 +1,5 @@
 // export const serverURL =
 //   (process.env.NEXT_PUBLIC_API_BASE_URL as string) ||
 //   "http://localhost:5000/api/v1";
-export const serverURL = "http://localhost:5000/api/v1"
+export const serverURL =
+  "https://elegance-interiors-backend-server-programmer-mehadi.vercel.app/api/v1"
